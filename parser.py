ec89dad8d19fbf4eb51082723fc377f9463a68c3
pollonium <pollonium9000@gmail.com>
2018-05-05 18:36:23 +0700

    turned code into module

--- parser.py
+++ parser.py
@@ -1,21 +1,26 @@
 # todo: add requirements.txt
 import urllib.request  # todo: обработать исключения типа 5xx, 4xx (см. https://khashtamov.com/ru/python-requests/)
+import random
 from bs4 import BeautifulSoup  # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 
-username = 'vankarish'
-base_url = 'https://www.livelib.ru/reader/' + username + '/wish/~'
-page_number = 1
-books = []
-while True:
-    url = base_url + str(page_number)
-    response = urllib.request.urlopen(url)
-    page = response.read().decode('utf-8')
-    soup = BeautifulSoup(page, "html.parser")
-    books_raw = soup.findAll("a", {"class": "brow-book-name"})
-    books_per_page = []
-    for book in books_raw:
-        books_per_page.append(book.get('title'))
-    if not len(books_per_page):
-        break
-    books += books_per_page
-    page_number += 1
+
+def get_book(username):
+    base_url = 'https://www.livelib.ru/reader/' + username + '/wish/~'
+    page_number = 1
+    books = []
+
+    while True:
+        url = base_url + str(page_number)
+        response = urllib.request.urlopen(url)
+        page = response.read().decode('utf-8')
+        soup = BeautifulSoup(page, "html.parser")
+        books_raw = soup.findAll("a", {"class": "brow-book-name"})
+        books_per_page = []
+        for book in books_raw:
+            books_per_page.append(book.get('title'))
+        if not len(books_per_page):
+            break
+        books += books_per_page
+        page_number += 1
+
+    return random.choice(books)
